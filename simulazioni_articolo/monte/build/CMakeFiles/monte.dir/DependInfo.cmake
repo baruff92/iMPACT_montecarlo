fc9529ccd0dffa6669e776800245499a982eb940
baruff92 <baruff92@gmail.com>
2023-10-10 11:52:47 +0200

    231010

--- simulazioni_articolo/monte/build/CMakeFiles/monte.dir/DependInfo.cmake
+++ simulazioni_articolo/monte/build/CMakeFiles/monte.dir/DependInfo.cmake
@@ -1,24 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/main.cc" "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/build/CMakeFiles/monte.dir/main.cc.o"
-  "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/src/calorimeter.cc" "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/build/CMakeFiles/monte.dir/src/calorimeter.cc.o"
-  "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/src/event_dictionary.cc" "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/build/CMakeFiles/monte.dir/src/event_dictionary.cc.o"
-  "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/src/fpga.cc" "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/build/CMakeFiles/monte.dir/src/fpga.cc.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/share/cern/root-install/include"
-  ".././include"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/main.cc" "CMakeFiles/monte.dir/main.cc.o" "gcc" "CMakeFiles/monte.dir/main.cc.o.d"
+  "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/src/calorimeter.cc" "CMakeFiles/monte.dir/src/calorimeter.cc.o" "gcc" "CMakeFiles/monte.dir/src/calorimeter.cc.o.d"
+  "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/src/event_dictionary.cc" "CMakeFiles/monte.dir/src/event_dictionary.cc.o" "gcc" "CMakeFiles/monte.dir/src/event_dictionary.cc.o.d"
+  "/home/gate/Desktop/iMPACT_montecarlo/simulazioni_articolo/monte/src/fpga.cc" "CMakeFiles/monte.dir/src/fpga.cc.o" "gcc" "CMakeFiles/monte.dir/src/fpga.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- simulazioni_articolo/monte/build/CMakeFiles/monte.dir/cmake_clean.cmake
+++ simulazioni_articolo/monte/build/CMakeFiles/monte.dir/cmake_clean.cmake
@@ -1,10 +1,14 @@
 file(REMOVE_RECURSE
   "CMakeFiles/monte.dir/main.cc.o"
+  "CMakeFiles/monte.dir/main.cc.o.d"
   "CMakeFiles/monte.dir/src/calorimeter.cc.o"
+  "CMakeFiles/monte.dir/src/calorimeter.cc.o.d"
   "CMakeFiles/monte.dir/src/event_dictionary.cc.o"
+  "CMakeFiles/monte.dir/src/event_dictionary.cc.o.d"
   "CMakeFiles/monte.dir/src/fpga.cc.o"
-  "monte.pdb"
+  "CMakeFiles/monte.dir/src/fpga.cc.o.d"
   "monte"
+  "monte.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
